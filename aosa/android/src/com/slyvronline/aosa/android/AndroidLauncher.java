b0a307cad703f94ec99d303f829d95a82bfd9fe7
Matthew Schrum <mschrum89@gmail.com>
2015-10-23 11:30:16 -0500

    Initial commit

---
+++ aosa/android/src/com/slyvronline/aosa/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.slyvronline.aosa.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.slyvronline.aosa.Aosa;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Aosa(), config);
+	}
+}
---
+++ aosa/desktop/src/com/slyvronline/aosa/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.slyvronline.aosa.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.slyvronline.aosa.Aosa;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Aosa(), config);
+	}
+}
